089387ee09731cd7f474042b0740967ddacbe02f
rweisshaar1 <robbieweisshaar@gmail.com>
2023-07-09 14:50:58 -0500

    recent updates

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ var userAnswers = document.querySelector(".userAnswers");
 // start button
 var button = document.querySelector(".submit");
 
-// submit button
+// submit buttons
 var buttonSubmit = document.getElementById("submit");
 buttonSubmit.setAttribute("style", "visibility: hidden")
 //timer
@@ -44,9 +44,10 @@ var currentScore
 var right = localStorage.getItem("right") // right answer counter
 
 var userCorrect = 0;
-var userIncorrect = 0;
+var userCorrectPoints = 1
 
-const correct = userCorrect ++;
+const correct = userCorrect += userCorrectPoints ;
+var userCorrectPointDisplay = correct
 
 
 button.textContent = "Start";
@@ -88,8 +89,9 @@ function startQuiz() {
 
   buttonSubmit;
   questionOne();
+  return
 }
-
+// asking question one
 function questionOne() {
   question.textContent = "1. This will be the first question of the quiz.";
 
@@ -106,33 +108,41 @@ function questionOne() {
   buttonSubmit.addEventListener("click", isAnswerRight1);
 }
 
+function clearRadio () {
+  radio1.checked = false;
+  radio2.checked = false;
+  radio3.checked = false;
+  radio4.checked = false;
+}
+// checking if answer one is correct
 function isAnswerRight1(){
   if (radio1.checked) { // correct one
-    localStorage.setItem("right", correct);
-    userAnswers.textContent = "Correct!"
+    correct
+    userAnswers.textContent = "Correct!" // correct one
     questionTwo();
-    console.log(userCorrect)
+    console.log(userCorrectPointDisplay) ;
 } else if (radio2.checked) {
   userAnswers.textContent = "Incorrect!"
+  console.log(userCorrectPointDisplay) ;
   questionTwo() ;
 } else if (radio3.checked) {
   userAnswers.textContent = "Incorrect!"
   questionTwo();
 } else if (radio4.checked) {
   userAnswers.textContent = "Incorrect!"
+  console.log(userCorrectPointDisplay) ;
   questionTwo() ;
 } else {
   userAnswers.textContent = "Please select an answer to continue."
+  console.log(userCorrectPointDisplay) ;
   questionOne();
 }
 }
 
-function questionTwo(){
-  radio1.checked = false;
-  radio2.checked = false;
-  radio3.checked = false;
-  radio4.checked = false;
 
+// function for second question
+function questionTwo() {
+  
   question.textContent = "2. This will be the second question of the quiz.";
 
   allAnswers = [
@@ -147,24 +157,166 @@ function questionTwo(){
   ]
   buttonSubmit.addEventListener("click", isAnswerRight2);
 }
-
+// checking if answer 2 is correct
 function isAnswerRight2(){
   if (radio1.checked) {
     userAnswers.textContent = "Incorrect!"
-    questionTwo();
-    console.log(userCorrect)
+    questionThree() ;
+    console.log(userCorrectPointDisplay)
 } else if (radio2.checked) { // correct one
-  localStorage.setItem("right", correct);
+  correct
   userAnswers.textContent = "Correct!"
-  questionTwo() ;
+  console.log(userCorrectPointDisplay)
+  questionThree() ;
 } else if (radio3.checked) {
   userAnswers.textContent = "Incorrect!"
-  questionTwo();
+  console.log(userCorrectPointDisplay)
+  questionThree() ;
 } else if (radio4.checked) {
   userAnswers.textContent = "Incorrect!"
-  questionTwo() ;
+  console.log(userCorrectPointDisplay)
+  questionThree() ;
 } else {
   userAnswers.textContent = "Please select an answer to continue."
-  questionOne();
-}
+  console.log(userCorrectPointDisplay)
+  questionTwo();
+}} ;
+
+// asking question three
+function questionThree() {
+  question.textContent = "3. This will be the third question of the quiz.";
+
+  allAnswers = [
+    {
+      info: answer1Text.textContent = "A. This is not the answer to question 3",},
+    {
+      info: answer2Text.textContent = "B. This is not the answer to question 3"},
+    {
+      info: answer3Text.textContent = "C. This is not the answer to question 3"},
+    {
+      info: answer4Text.textContent = "D. This is the answer to question 3"}
+    ]
+  buttonSubmit.addEventListener("click", isAnswerRight3);
+} ;
+
+function isAnswerRight3(){
+  if (radio1.checked) { // correct one
+    userAnswers.textContent = "Incorrect!"
+    questionFour();
+    console.log(userCorrectPointDisplay) ;
+} else if (radio2.checked) {
+  userAnswers.textContent = "Incorrect!"
+  console.log(userCorrectPointDisplay) ;
+  questionFour();
+} else if (radio3.checked) {
+  userAnswers.textContent = "Incorrect!"
+  console.log(userCorrectPointDisplay) ;
+  questionFour();
+} else if (radio4.checked) {
+  userAnswers.textContent = "Correct!" // correct one
+  correct
+  console.log(userCorrectPointDisplay) ;
+  questionFour();
+} else {
+  userAnswers.textContent = "Please select an answer to continue."
+  console.log(userCorrectPointDisplay) ;
+  questionThree();
+}}
+
+//question four function
+function questionFour() {
+
+  question.textContent = "4. This will be the fourth question of the quiz.";
+
+  allAnswers = [
+    {
+      info: answer1Text.textContent = "A. This is the answer to question 4"},
+    {
+      info: answer2Text.textContent = "B. This is not the answer to question 4"},
+    {
+      info: answer3Text.textContent = "C. This is not the answer to question 4"},
+    {
+      info: answer4Text.textContent = "D. This is not the answer to question 4"}
+    ]
+  buttonSubmit.addEventListener("click", isAnswerRight4);
+} ;
+
+// checking if answer 4 is right
+function isAnswerRight4(){
+  if (radio1.checked) { // correct one
+    userAnswers.textContent = "Correct!" // correct one
+    correct
+    questionFive();
+    console.log(userCorrectPointDisplay) ;
+} else if (radio2.checked) {
+  userAnswers.textContent = "Incorrect!"
+  console.log(userCorrectPointDisplay) ;
+  questionFive();
+} else if (radio3.checked) {
+  userAnswers.textContent = "Incorrect!"
+  console.log(userCorrectPointDisplay) ;
+  questionFive();
+} else if (radio4.checked) {
+  userAnswers.textContent = "Incorrect!"
+  console.log(userCorrectPointDisplay) ;
+  questionFive();
+} else {
+  userAnswers.textContent = "Please select an answer to continue."
+  console.log(userCorrectPointDisplay) ;
+  questionFour();
+}} ;
+
+// displays question 5
+function questionFive() {
+
+  question.textContent = "5. This will be the fifth question of the quiz.";
+
+  allAnswers = [
+    {
+      info: answer1Text.textContent = "A. This is not the answer to question 4"},
+    {
+      info: answer2Text.textContent = "B. This is not the answer to question 4"},
+    {
+      info: answer3Text.textContent = "C. This is the answer to question 4"},
+    {
+      info: answer4Text.textContent = "D. This is not the answer to question 4"}
+    ]
+  buttonSubmit.addEventListener("click", isAnswerRight5);
+} ;
+
+// checking if answer 5 is correct
+function isAnswerRight5 () {
+  if (radio1.checked) { 
+    userAnswers.textContent = "Incorrect!"
+    showScore();
+    console.log(userCorrectPointDisplay) ;
+} else if (radio2.checked) {
+  userAnswers.textContent = "Incorrect!"
+  console.log(userCorrectPointDisplay) ;
+  showScore();
+} else if (radio3.checked) {
+  userAnswers.textContent = "Correct!" // correct one
+  correct
+  console.log(userCorrectPointDisplay) ;
+  showScore();
+} else if (radio4.checked) {
+  userAnswers.textContent = "Incorrect!"
+  console.log(userCorrectPointDisplay) ;
+  showScore();
+} else {
+  userAnswers.textContent = "Please select an answer to continue."
+  console.log(userCorrectPointDisplay) ;
+  questionFive();
+}} ;
+
+// shows score page
+function showScore () {
+  var scoreMultiplier = 20 ;
+  const actualScore = userCorrectPointDisplay *= scoreMultiplier ;
+
+  console.log(userCorrectPointDisplay,scoreMultiplier);
+  console.log(actualScore)
+
+  answers.setAttribute("style", "visibility: hidden") ;
+
 }
